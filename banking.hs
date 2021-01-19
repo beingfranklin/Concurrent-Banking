3bec85863f99359756d727fd905e9247b0544e13
Franklin Antony <franklinanton.007@gmail.com>
2021-01-19 14:10:39 +0000

    Updated

--- banking.hs
+++ banking.hs
@@ -6,11 +6,14 @@ import System.Random
 
 import qualified Data.Map as Map
 
+-- | Customer Datatype
 type AccountName = Int
-type Account = TVar Pound
+-- | type AccountNo = Int
 type Pound = Integer
-type Bank = TVar (Map.Map AccountName Account)
+type Account = TVar Pound
+type Customer = TVar (Map.Map AccountName Account)
 
+-- | Constants
 numberOfAccounts = 10
 threads = 10
 transactionsPerThread = 100
@@ -18,18 +21,18 @@ initialValue = 1000
 minAmount = 10
 maxAmount = 50
 
--- Get account by ID, create new empty account if it didn't exist
-getAccount :: Bank -> AccountName -> STM Account
-getAccount bank accountId = do
-  accounts <- readTVar bank
+-- | Get account by ID, create new empty account if it didn't exist
+getAccount :: Customer -> AccountName -> STM Account
+getAccount customer accountId = do
+  accounts <- readTVar customer
   case Map.lookup accountId accounts of
     Just account -> return account
     Nothing -> do
       account <- newTVar initialValue
-      writeTVar bank $ Map.insert accountId account accounts
+      writeTVar customer $ Map.insert accountId account accounts
       return account
 
--- Transfer amount between two accounts
+-- | Transfer amount between two accounts
 transfer :: Pound -> Account -> Account -> STM ()
 transfer amount from to = when (from /= to) $ do
   balanceFrom <- readTVar from
@@ -40,8 +43,9 @@ transfer amount from to = when (from /= to) $ do
     writeTVar from $! balanceFrom - amount
     writeTVar to $! balanceTo + amount
 
-randomTransaction :: Bank -> IO ()
-randomTransaction bank = do
+-- | Generate random transaction
+randomTransaction :: Customer -> IO ()
+randomTransaction customer = do
   -- Make a random transaction
   fromId <- randomRIO (1, numberOfAccounts)
   toId   <- randomRIO (1, numberOfAccounts)
@@ -49,17 +53,18 @@ randomTransaction bank = do
 
   -- Perform it atomically
   atomically $ do
-    from <- getAccount bank fromId
-    to   <- getAccount bank toId
+    from <- getAccount customer fromId
+    to   <- getAccount customer toId
     transfer amount from to
-
+-- | Main function
+main :: IO ()
 main = do
-  bank <- newTVarIO Map.empty
-  -- Starting  worker threads to do a number of random transactions
+  customer <- newTVarIO Map.empty
+  -- Starting worker threads to do a number of random transactions
   workers <- replicateM threads $ do
     done <- newEmptyMVar
     forkIO $ do
-      replicateM_ transactionsPerThread $ randomTransaction bank
+      replicateM_ transactionsPerThread $ randomTransaction customer
       putMVar done ()
     return done
 
@@ -69,7 +74,7 @@ main = do
   -- Print list of accounts and total bank balance
   putStrLn "----------------"
   summary <- atomically $ do
-    accounts <- readTVar bank
+    accounts <- readTVar customer
     forM (Map.assocs accounts) $ \(accountId, account) -> do
       balance <- readTVar account
       return (accountId, balance)
